33c26dbe72a74288840a80ec4dae479bde10c474
Fuad Hasanli <fhesenli92@gmail.com>
2025-03-07 16:17:24 +0400

    changes

---
+++ resources/views/includes/hero.blade.php
@@ -0,0 +1,41 @@
+<!-- Hero Section -->
+<section id="hero" class="hero section">
+    <div id="hero-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
+        <div class="carousel-item active">
+            <img src="{{ asset('assets/img/hero-carousel/hero-carousel-1.jpg') }}" alt="">
+            <div class="container">
+                <h2>Welcome to Medicio</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a href="#about" class="btn-get-started">Read More</a>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <img src="{{ asset('assets/img/hero-carousel/hero-carousel-2.jpg') }}" alt="">
+            <div class="container">
+                <h2>At vero eos et accusamus</h2>
+                <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.</p>
+                <a href="#about" class="btn-get-started">Read More</a>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <img src="{{ asset('assets/img/hero-carousel/hero-carousel-3.jpg') }}" alt="">
+            <div class="container">
+                <h2>Temporibus autem quibusdam</h2>
+                <p>Beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.</p>
+                <a href="#about" class="btn-get-started">Read More</a>
+            </div>
+        </div>
+
+        <a class="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+        </a>
+
+        <a class="carousel-control-next" href="#hero-carousel" role="button" data-bs-slide="next">
+            <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+        </a>
+
+        <ol class="carousel-indicators"></ol>
+    </div>
+</section> 
